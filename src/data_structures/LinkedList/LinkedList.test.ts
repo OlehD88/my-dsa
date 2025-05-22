c1afab5f8fd2781f94669ebcb1bfd99c3824029d
OlehD88 <demchukoleg88@gmail.com>
2025-05-22 21:27:06 +0200

    Add Linked list

---
+++ src/data_structures/LinkedList/LinkedList.test.ts
@@ -0,0 +1,92 @@
+import { LinkedList } from './LinkedList'
+
+describe('Linked List', () => {
+	it('Should create a list, insert a new node and read it', () => {
+		const list = new LinkedList()
+		list.insertToHead(10)
+
+		expect(list.read(0)).toEqual(10)
+		expect(list.getHead()).toEqual(10)
+		expect(list.getTail()).toEqual(10)
+	})
+
+	it('Should create a list, add 2 nodes and read head and tail', () => {
+		const list = new LinkedList()
+		list.insertToHead(10)
+		expect(list.read(0)).toEqual(10)
+		expect(list.getHead()).toEqual(10)
+		expect(list.getTail()).toEqual(10)
+
+		list.insertToTail(20)
+		expect(list.getHead()).toEqual(10)
+		expect(list.read(0)).toEqual(10)
+		expect(list.getTail()).toEqual(20)
+		expect(list.read(1)).toEqual(20)
+
+		expect(list.toArray()).toEqual([10, 20])
+	})
+
+	it('Should create a list with three nodes', () => {
+		const list = new LinkedList()
+		list.insertToHead(10)
+		expect(list.read(0)).toEqual(10)
+		expect(list.getHead()).toEqual(10)
+		expect(list.getTail()).toEqual(10)
+
+		list.insertToTail(20)
+		expect(list.getHead()).toEqual(10)
+		expect(list.read(0)).toEqual(10)
+		expect(list.getTail()).toEqual(20)
+		expect(list.read(1)).toEqual(20)
+
+		list.insert(30, 1)
+		expect(list.getHead()).toEqual(10)
+		expect(list.getTail()).toEqual(20)
+		expect(list.read(1)).toEqual(30)
+
+		expect(list.toArray()).toEqual([10, 30, 20])
+	})
+
+	it('Should handle delete of nodes', () => {
+		const list = new LinkedList()
+		list.insertToHead(10)
+		list.insertToTail(20)
+		list.insert(30, 1)
+
+		expect(list.toArray()).toEqual([10, 30, 20])
+
+		list.delete(0)
+		expect(list.getHead()).toEqual(30)
+		expect(list.getTail()).toEqual(20)
+
+		expect(list.toArray()).toEqual([30, 20])
+
+		list.insertToHead(10)
+
+		list.delete(2)
+		expect(list.getHead()).toEqual(10)
+		expect(list.getTail()).toEqual(30)
+
+		expect(list.toArray()).toEqual([10, 30])
+
+		list.insertToTail(20)
+
+		list.delete(1)
+		expect(list.getHead()).toEqual(10)
+		expect(list.getTail()).toEqual(20)
+
+		expect(list.toArray()).toEqual([10, 20])
+	})
+
+	it('Should properly search by value', () => {
+		const list = new LinkedList()
+		list.insertToHead(10)
+		list.insertToTail(20)
+		list.insert(30, 1)
+
+		expect(list.search(10)).toEqual(0)
+		expect(list.search(20)).toEqual(2)
+		expect(list.search(30)).toEqual(1)
+		expect(list.search(50)).toEqual(-1)
+	})
+})
---
+++ src/data_structures/LinkedList/LinkedList.ts
@@ -0,0 +1,168 @@
+type Node<T> = {
+	value: T
+	next?: Node<T>
+}
+
+export class LinkedList<T> {
+	public length: number = 0
+	public head?: Node<T>
+	public tail?: Node<T>
+
+	private createNode(value: T): Node<T> {
+		return {
+			value,
+		}
+	}
+
+	public insert(value: T, index: number) {
+		const newNode = this.createNode(value)
+
+		if (index === 0) {
+			this.insertToHead(value)
+			return
+		}
+
+		if (index === this.length) {
+			this.insertToTail(value)
+			return
+		}
+
+		const prevIndexNode = this.getNodeAt(index - 1)
+
+		if (!prevIndexNode) {
+			throw Error('Wrong index provided')
+		}
+
+		const nextNode = prevIndexNode?.next
+		prevIndexNode.next = newNode
+		newNode.next = nextNode
+
+		this.length++
+	}
+
+	public insertToHead(value: T) {
+		const newNode = this.createNode(value)
+		this.length++
+
+		if (!this.tail || !this.head) {
+			this.tail = newNode
+			this.head = newNode
+			return
+		}
+
+		newNode.next = this.head
+		this.head = newNode
+	}
+
+	public insertToTail(value: T) {
+		const newNode = this.createNode(value)
+		this.length++
+
+		if (!this.tail || !this.head) {
+			this.tail = newNode
+			this.head = newNode
+			return
+		}
+
+		this.tail.next = newNode
+
+		this.tail = newNode
+	}
+
+	private getNodeAt(index: number): Node<T> | null {
+		if (index >= this.length) {
+			throw new Error('Index is bigger than length of the list')
+		}
+
+		if (index === 0 && this.head) {
+			return this.head
+		}
+
+		if (index === this.length - 1 && this.tail) {
+			return this.tail
+		}
+
+		let currNode = this.head?.next
+		let currIndex = 1
+
+		while (currIndex < index && currNode) {
+			currIndex += 1
+			currNode = currNode.next
+		}
+
+		return currNode || null
+	}
+
+	public read(index: number): T | null {
+		const node = this.getNodeAt(index)
+		return node?.value || null
+	}
+
+	public getHead(): T | null {
+		return this.head?.value || null
+	}
+
+	public getTail(): T | null {
+		return this.tail?.value || null
+	}
+
+	public toArray(): T[] {
+		const list: T[] = []
+
+		let currNode = this.head
+		while (currNode) {
+			list.push(currNode.value)
+			currNode = currNode.next
+		}
+
+		return list
+	}
+
+	public delete(index: number) {
+		if (this.length <= index) {
+			throw new Error('Index is bigger than length of the list')
+		}
+
+		if (this.length === 1) {
+			this.head = undefined
+			this.tail = undefined
+			this.length--
+			return
+		}
+
+		if (index === 0 && this.head) {
+			this.head = this.head.next
+		} else {
+			const prevNodeToDelete = this.getNodeAt(index - 1)
+			const nodeToDelete = prevNodeToDelete?.next
+			if (!nodeToDelete) {
+				throw Error('Wrong index provided')
+			}
+
+			prevNodeToDelete.next = nodeToDelete.next
+			if (index === this.length - 1) {
+				this.tail = prevNodeToDelete
+			}
+		}
+
+		this.length--
+	}
+
+	public search(value: T): number {
+		if (this.length === 0) return -1
+
+		let currNode = this.head
+		let currIndex = 0
+
+		while (currNode) {
+			if (currNode.value === value) {
+				return currIndex
+			}
+
+			currIndex++
+			currNode = currNode.next
+		}
+
+		return -1
+	}
+}
